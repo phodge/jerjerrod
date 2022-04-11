454f3f0f019808762aedb6e8c8739fda584e21ee
Peter Hodge <peter.hodge84@gmail.com>
2022-04-12 09:18:31 +1000

    Formatting to force PR job to run

--- jerjerrod/__init__.py
+++ jerjerrod/__init__.py
@@ -1,3 +1,3 @@
 # This constant should be updated dynamically by the poetry-dynamic-versioning
 # plugin, see additional configuration in pyproject.toml
-__version__ = '0.0.0'
+__version__ = "0.0.0"
